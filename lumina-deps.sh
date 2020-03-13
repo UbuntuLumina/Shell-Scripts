8b76d49963eb50d966df9f5547731890bd498adf
Kai <lyons.safyre@gmail.com>
2020-03-13 16:58:21 -0600

    Update lumina-deps.sh

--- lumina-deps.sh
+++ lumina-deps.sh
@@ -1,4 +1,8 @@
-sudo apt-get install build-essential qt5-default qttools5-dev-tools libqt5gui5 qtmultimedia5-dev libqt5multimediawidgets5  \
-libqt5network5 libqt5svg5-dev libqt5x11extras5-dev libxcb-icccm4-dev libxcb-ewmh-dev libxcb-composite0-dev libxcb-damage0-dev  \
-libxcb-util0-dev libphonon-dev libxcomposite-dev libxdamage-dev libxrender-dev libxcb-image0-dev libxcb-screensaver0-dev  \ 
-qtdeclarative5-dev fluxbox kde-style-oxygen xscreensaver xbacklight alsa-utils acpi numlockx pavucontrol xterm sysstat -y
+sudo apt install build-essential qt5-default qttools5-dev-tools 
+sudo apt install libqt5gui5 qtmultimedia5-dev libqt5multimediawidgets5
+sudo apt install libqt5network5 libqt5svg5-dev libqt5x11extras5-dev libxcb-icccm4-dev 
+sudo apt install libxcb-ewmh-dev libxcb-composite0-dev libxcb-damage0-dev
+sudo apt install libxcb-util0-dev libphonon-dev libxcomposite-dev libxdamage-dev 
+sudo apt install libxrender-dev libxcb-image0-dev libxcb-screensaver0-dev
+sudo apt install qtdeclarative5-dev fluxbox kde-style-oxygen xscreensaver xbacklight 
+sudo apt install alsa-utils acpi numlockx pavucontrol xterm sysstat libxcursor -y
