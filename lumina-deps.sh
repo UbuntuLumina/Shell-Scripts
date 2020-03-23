ed52b37770b34a10d25c99fa17699b5a639f9de1
Kai <lyons.safyre@gmail.com>
2020-03-23 09:27:47 -0600

    Make sure all are autoran, add missing deps

--- lumina-deps.sh
+++ lumina-deps.sh
@@ -1,8 +1,9 @@
-sudo apt install build-essential qt5-default qttools5-dev-tools 
-sudo apt install libqt5gui5 qtmultimedia5-dev libqt5multimediawidgets5
-sudo apt install libqt5network5 libqt5svg5-dev libqt5x11extras5-dev libxcb-icccm4-dev 
-sudo apt install libxcb-ewmh-dev libxcb-composite0-dev libxcb-damage0-dev
-sudo apt install libxcb-util0-dev libphonon-dev libxcomposite-dev libxdamage-dev 
-sudo apt install libxrender-dev libxcb-image0-dev libxcb-screensaver0-dev
-sudo apt install qtdeclarative5-dev fluxbox kde-style-oxygen xscreensaver xbacklight 
+sudo apt install build-essential qt5-default qttools5-dev-tools libxrandr-dev -y
+sudo apt install libqt5gui5 qtmultimedia5-dev libqt5multimediawidgets5 -y
+sudo apt install libqt5network5 libqt5svg5-dev libqt5x11extras5-dev libxcb-icccm4-dev -y
+sudo apt install libxcb-ewmh-dev libxcb-composite0-dev libxcb-damage0-dev -y
+sudo apt install libxcb-util0-dev libphonon-dev libxcomposite-dev libxdamage-dev -y
+sudo apt install libxrender-dev libxcb-image0-dev libxcb-screensaver0-dev -y
+sudo apt install qtdeclarative5-dev fluxbox kde-style-oxygen xscreensaver xbacklight -y
 sudo apt install alsa-utils acpi numlockx pavucontrol xterm sysstat libxcursor -y
+sudo apt install libqt5concurrent5 libqt5core5a -y
