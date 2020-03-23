5efcb6a97b71d79462e3661b8727ca0a7140061f
Kai <lyons.safyre@gmail.com>
2020-03-23 11:39:55 -0600

    Update install-calamares.sh

--- install-calamares.sh
+++ install-calamares.sh
@@ -1,4 +1,6 @@
 sudo wget https://github.com/calamares/calamares/releases/download/v3.2.20/calamares-3.2.20.tar.gz
 cd calamares-3.2.20
-
-# Build Calamares by hand rest of the way for now
+sudo mkdir build
+cd build
+sudo cmake ..
+make
